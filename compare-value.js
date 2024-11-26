d3f7a7ba47ae094cb6894a9bb366e66d66ce5b1a
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-11-27 03:00:20 +0600

    added something

--- compare-value.js
+++ compare-value.js
@@ -1,9 +1,6 @@
 const compareArr = (a, b) => {
-  let value = a.length === b.length && a.every((v, i) =>
-    v === b[i])
-  return value;
-
-
+  const value = a.length === b.length && a.every((v, i) => v === b[i])
+  return value
 }
 
 const arr = compareArr([1, 2, 3], [1, 2, 3])
---
+++ shuffle.js
@@ -0,0 +1,3 @@
+let shuffle = [1, 2, 3, 4, 5, 6]
+const arr = shuffle.sort(() => Math.random() - 0.5)
+console.log(arr)
