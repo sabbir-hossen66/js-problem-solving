97bdc12a6eefe2a55e091ac53dd92a224f65cef8
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-11-29 00:56:18 +0600

    added problem solving

---
+++ commitment.js
@@ -0,0 +1,11 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+
+for (let number = 1; number < 61; number++) {
+  const element = `I will invest at least 6 hrs every single day for!${number}`;
+  console.log(element)
+}
---
+++ odd.js
@@ -0,0 +1,15 @@
+//Find all the odd numbers from 61 to 100.
+for (let num = 61; num < 100; num++) {
+  if (num % 2 !== 0) {
+    // console.log(num)
+  }
+}
+
+//Find all the even numbers from 78 to 98.
+
+for (let num = 78; num < 99; num++) {
+  if (num % 2 == 0) {
+    console.log(num)
+  }
+
+}
