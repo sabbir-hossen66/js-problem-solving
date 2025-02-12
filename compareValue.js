e38ac320bb472cf5a1502d3a468523a283385a22
Sabbir Hossen <Sabbir Hossen>
2025-02-13 02:14:15 +0600

    added new solution

---
+++ compareValue.js
@@ -0,0 +1,26 @@
+// const compareValue = () => {
+//   num1 = 5;
+//   num2 = 7;
+//   if (num1 == num2) {
+//     console.log('values are same')
+//   }
+//   else {
+//     console.log('values are not same');
+    
+//   }
+// }
+
+// compareValue()
+
+const compareValue = (num1,num2) => {
+ 
+  if (num1 == num2) {
+    console.log('values are same')
+  }
+  else {
+    console.log('values are not same');
+    
+  }
+}
+
+compareValue(4,8)
