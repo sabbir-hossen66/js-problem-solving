9dac6020241a52daadf2cb93d06b171a45e05249
Sabbir Hossen <Sabbir Hossen>
2025-02-15 23:32:42 +0600

    odd or even numbers

--- for.js
+++ for.js
@@ -1,4 +1,18 @@
-for (let i = 0; i < array.length; i++) {
-  const element = array[i];
 
+
+//Odd or Even: Check if a number is odd or even and print the result
+
+let number=[1,2,3,4,5,6,7,8,9,10]
+for (let i = 0; i <=number.length; i++) {
+
+
+  if (number[i] % 2 == 0){
+    console.log('even number: ',number[i]);
+    
+  }
+  
+  else {
+    console.log('number are odd: ', number[i]);
+    
+  }
 }
