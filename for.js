db23d4de54ca7ba7a39e82fd46e9db72b817be1a
Sabbir Hossen <Sabbir Hossen>
2025-02-18 00:10:51 +0600

    added new

--- for.js
+++ for.js
@@ -2,20 +2,20 @@
 
 //Odd or Even: Check if a number is odd or even and print the result
 
-// let number=[1,2,3,4,5,6,7,8,9,10]
-// for (let i = 0; i <=number.length; i++) {
+let number=[1,2,3,4,5,6,7,8,9,10]
+for (let i = 0; i <=number.length; i++) {
 
 
-//   if (number[i] % 2 == 0){
-//     console.log('even number: ',number[i]);
+  if (number[i] % 2 == 0){
+    console.log('even number: ',number[i]);
     
-//   }
+  }
   
-//   else {
-//     console.log('number are odd: ', number[i]);
+  else {
+    console.log('number are odd: ', number[i]);
     
-//   }
-// }
+  }
+}
 
 
 //3) Max of Three Numbers: Write a program that takes three numbers as input and outputs the largest one.
---
+++ new.js
@@ -0,0 +1,24 @@
+// 7) Divisibility Test: Check whether a number is divisible by both 3 and 5.
+
+function divisible(n) {
+  if ((n % 3 == 0) && (n % 5 == 0)) {
+    return `this is ${n}`
+  }
+  else {
+    return `sorry bro `
+  }
+}
+
+const res = divisible(15)
+// console.log(res);
+
+// Sum of Digits: Write a program to find the sum of the digits of a given number.
+function sum(n) {
+   const numStr = n.toString();
+  let num = 0;
+  for (let i = 0; i < numStr.length; i++){
+   num += parseInt(numStr[i]);
+  }
+  return num
+}
+console.log(sum(3456));
